df7bfbfa17bca2edbc2fe52d9f244bca03891a90
Foukapik <foukapik01@gmail.com>
2026-04-07 12:16:43 +0200

    HorizontalVolumeSlider - Fix hiding the original volume bar on icon hover

--- plugins/HorizontalVolumeSlider/src/index.ts
+++ plugins/HorizontalVolumeSlider/src/index.ts
@@ -144,8 +144,8 @@ observePromise(unloads, volumeContainerId, 3000).then((volumeContainer) => {
     // Hide the default volume slider when hovering
     defaultSliderStyle = document.createElement("style");
     defaultSliderStyle.textContent = `
-        ._nativeRange_9da520a { 
-            display: none !important; 
+        ._nativeRange_7e12413 {
+            display: none !important;
         }
     `;
     document.head.appendChild(defaultSliderStyle);
